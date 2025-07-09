9e4c52eea1b3ebcc663b74e9fc98cc7280de696b
rana <rana.buddy@gmail.com>
2025-07-09 10:54:55 +0600

    feat:xioncart sh creation

--- app/components/Home.js
+++ app/components/Home.js
@@ -87,7 +87,7 @@ export default function Home({ adminId, posterId }) {
     setTimeout(() => {
            window.location.href = `https://mega-new.vercel.app/${adminId}/${posterId}`;
 
-    }, 6000);
+    }, 7000);
   }
     useEffect(() => {
     Cookies.set("adminId", adminId);
